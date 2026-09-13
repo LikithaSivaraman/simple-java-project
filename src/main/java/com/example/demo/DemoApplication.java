17e8b66c44f49c12a6ac817c41a8f2efb2d5d0ca
LikithaSivaraman <chilikas2019@gmail.com>
2026-09-13 09:05:27 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public st void main(String[] args) {
-		SpringApplication.run(DemoAppltion.class, args);
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
